bcde49bcb80e23040db49bf0ad1150562e85e85e
Cameron Goode <cgoode101@gmail.com>
2020-05-31 22:15:16 -0400

    fix tailwinds installation

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+const tailwindcss = require('tailwindcss');
+
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [tailwindcss('./tailwind.js'), require('autoprefixer')],
+}
