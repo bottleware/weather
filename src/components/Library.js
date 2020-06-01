16f6a3d5fbaa6b9be96c12c1703f2c5d3cdf9993
Cameron Goode <cgoode101@gmail.com>
2020-06-01 19:57:40 -0400

    switch to https

--- src/components/Library.js
+++ src/components/Library.js
@@ -11,8 +11,6 @@ function Library() {
   let [country, setCountry] = useState();
   let [tempUnit, setTempUnit] = useState(false);
 
-  const selectClass = "bg-gray-500 hover:bg-black-700 text-white font-bold py-2 px-4 rounded w-100 mr-10";
-
   const weather = {
     city,
     state,
@@ -26,11 +24,11 @@ function Library() {
         throw new Error('Need a city..');
       }
       if(search.state && search.country) {
-        response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search.city},${search.state},${search.country}&appid=${key}`);
+        response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.city},${search.state},${search.country}&appid=${key}`);
       } else if(search.state) {
-        response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search.city},${search.state}&appid=${key}`);
+        response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.city},${search.state}&appid=${key}`);
       } else {
-        response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search.city}&appid=${key}`);
+        response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.city}&appid=${key}`);
       }
 
       setWeatherData(await response.json());
@@ -61,15 +59,15 @@ function Library() {
         >
                 Search
         </button>  
-        <div class="inline-flex">
+        <div className="inline-flex">
             <button
-              class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+              className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
               onClick={() => setTempUnit(true)}
             >
               F
             </button>
             <button
-              class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+              className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
               onClick={() => setTempUnit(false)}
             >
               C 
