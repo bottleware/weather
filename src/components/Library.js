ed15d94bc44d12ac0f1219a8e862f906d4a1d54f
Cameron Goode <cgoode101@gmail.com>
2020-06-02 13:58:35 -0400

    lint files

--- src/components/Library.js
+++ src/components/Library.js
@@ -4,74 +4,73 @@ import WeatherDisplay from './WeatherDisplay';
 import key from '../key';
 
 function Library() {
-
-  let [weatherData, setWeatherData] = useState();
-  let [city, setCity] = useState();
-  let [state, setState] = useState();
-  let [country, setCountry] = useState();
-  let [tempUnit, setTempUnit] = useState(false);
+  const [weatherData, setWeatherData] = useState();
+  const [city, setCity] = useState();
+  const [state, setState] = useState();
+  const [country, setCountry] = useState();
+  const [tempUnit, setTempUnit] = useState(false);
 
   const weather = {
     city,
     state,
-    country
+    country,
   };
 
+  // search is an object of the form {city, state, country}
   const getWeather = async (search) => {
     let response;
     try {
-      if(!search.city) {
+      if (search.city === "") {
         throw new Error('Need a city..');
       }
-      if(search.state && search.country) {
+      if (search.state && search.country) {
         response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.city},${search.state},${search.country}&appid=${key}`);
-      } else if(search.state) {
+      } else if (search.state) {
         response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.city},${search.state}&appid=${key}`);
       } else {
         response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.city}&appid=${key}`);
       }
 
       setWeatherData(await response.json());
-
-    } catch(error) {
-      console.log(error);
+    } catch (error) {
     }
-  }
+  };
 
   const clickSearch = () => {
     getWeather(weather);
-  }
+  };
 
   return (
     <div className="App ml-5">
-      <div id="header" 
+      <div
+        id="header"
         className="flex justify-start"
-        >
-        <SearchForm 
+      >
+        <SearchForm
           setCity={setCity}
           setState={setState}
           setCountry={setCountry}
         />
-        <button 
+        <button
           type="submit"
           className="bg-gray-500 hover:bg-black-700 text-white font-bold py-2 px-4 rounded w-100 mr-10"
           onClick={() => clickSearch()}
         >
-                Search
-        </button>  
+          Search
+        </button>
         <div className="inline-flex">
-            <button
-              className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
-              onClick={() => setTempUnit(true)}
-            >
-              F
-            </button>
-            <button
-              className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
-              onClick={() => setTempUnit(false)}
-            >
-              C 
-            </button>
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+            onClick={() => setTempUnit(true)}
+          >
+            F
+          </button>
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+            onClick={() => setTempUnit(false)}
+          >
+            C
+          </button>
         </div>
       </div>
       <WeatherDisplay weather={weatherData} unit={tempUnit}/>
--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -1,24 +1,37 @@
 import React from 'react';
 
-const Temperature = props => {
-
-    const kToTemp = kelvinTemp => {
-        if(props.unit === true) {
-            return Math.round(((kelvinTemp - 273.15)*9/5)+32)
-        } else {
-            return Math.round(kelvinTemp - 273.15)
-        }
+const Temperature = (props) => {
+  const kToTemp = (kelvinTemp) => {
+    if (props.unit === true) {
+      return Math.round(((kelvinTemp - 273.15) * 9 / 5) + 32);
     }
+    return Math.round(kelvinTemp - 273.15);
+  };
 
-    const tempSymbol = (props.unit === true ? 'F' : 'C');
+  const tempSymbol = (props.unit === true ? 'F' : 'C');
 
-    return (
-        <div>
-            <p>Temperature: {kToTemp(props.weather.main.temp)}°{tempSymbol}</p>
-            <p>Max temperature: {kToTemp(props.weather.main.temp_max)}°{tempSymbol}</p>
-            <p>Min temperature: {kToTemp(props.weather.main.temp_min)}°{tempSymbol}</p>
-        </div>
-    );
-}
+  return (
+    <div>
+      <p>
+        Temperature:
+        {kToTemp(props.weather.main.temp)}
+        °
+        {tempSymbol}
+      </p>
+      <p>
+        Max temperature:
+        {kToTemp(props.weather.main.temp_max)}
+        °
+        {tempSymbol}
+      </p>
+      <p>
+        Min temperature:
+        {kToTemp(props.weather.main.temp_min)}
+        °
+        {tempSymbol}
+      </p>
+    </div>
+  );
+};
 
-export default Temperature
+export default Temperature;
