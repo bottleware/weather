79fff27ac48665a00e792ad0a9643fb64c8c634e
Cameron Goode <cgoode101@gmail.com>
2020-06-02 13:59:32 -0400

    add error checking

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -3,11 +3,13 @@ import Temperature from './Temperature';
 import Message from './Message';
 
 const WeatherDisplay = (props) => {
-    if(props.weather !== undefined) {
-        return <Temperature weather={props.weather} unit={props.unit}/>
-    } else {
-        return <Message/>
-    }
-}
+  if (props.weather && props.weather.cod !== 200) {
+    return <Message message="Error! Location not found. Try being more specific or try another location."/>;
+  }
+  if (props.weather !== undefined) {
+    return <Temperature weather={props.weather} unit={props.unit}/>;
+  } 
+  return <Message message="Enter a city to see today's weather."/>;
+};
 
-export default WeatherDisplay 
+export default WeatherDisplay;
