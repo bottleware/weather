dd2b87db2e1d416f41b93f4d69537b72c3dbcf8f
cam <cam@pop-os.localdomain>
2020-07-02 14:10:53 -0400

    add opencage key

--- src/key.js
+++ src/key.js
@@ -1,4 +1,5 @@
 const openweatherKey = "af1f5096272ab26f772fbb15f70facab";
 const weatherbitKey = "96933722c6cc47b9804531e9b6a035b5";
-const keys = {openweatherKey, weatherbitKey};
+const opencage = "e873db7372274deebf40f7504d614e92";
+const keys = {openweatherKey, weatherbitKey, opencage};
 export default keys;
